ce315c44226fd6e701e92d2592fcfa44e77bddfd
masatsura1515 <masatsura1515@gmail.com>
2023-11-06 22:51:49 +0900

    “migration変更済み”

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(email: "admin@a",
+User.create!(email: "admin@b",
              password:  "111111",
-             password_confirmation: "111111")
+             password_confirmation: "111111",
+             admin: "true")
 
